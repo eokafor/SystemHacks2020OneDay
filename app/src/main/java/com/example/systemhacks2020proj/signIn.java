fb76410890fcf33d9abb81138d05922753456be6
Jordan Chong <jtchong@sfu.ca>
2020-02-08 17:13:23 -0800

    fixed

--- app/src/main/java/com/example/systemhacks2020proj/signIn.java
+++ app/src/main/java/com/example/systemhacks2020proj/signIn.java
@@ -16,6 +16,7 @@ public class signIn extends AppCompatActivity {
     private EditText password;
     private Button login;
     private TextView info;
+    private Button suned;
     private int count = 5;
 
     @Override
@@ -26,16 +27,26 @@ public class signIn extends AppCompatActivity {
         email = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.btnLogin);
-        info = (TextView) findViewById(R.id.textView);
+        info = (TextView) findViewById(R.id.incorrectInput);
+        suned = (Button) findViewById(R.id.btnNever);
 
 
         login.setEnabled(true);
+        suned.setEnabled(true);
 
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 validate(email.getText().toString(), password.getText().toString());
+
+            }
+        });
+
+        suned.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crest();
             }
         });
 
@@ -45,16 +56,21 @@ public class signIn extends AppCompatActivity {
 
     private void validate (String myEmail, String myPassword )
     {
+        TextView textview = findViewById(R.id.incorrectInput);
         if((myEmail .equals("Agnes")) && (myPassword .equals("1234")))
         {
             Intent intent = new Intent(signIn.this, MainActivity.class);
             startActivity(intent);
+            textview.setText("");
+
         }
         else
         {
+
+            textview.setText("Incorrect password/email");
             count--;
 
-            info.setText("Incorrect password/email:" + String.valueOf(count));
+
 
             if(count == 0)
             {
@@ -63,7 +79,11 @@ public class signIn extends AppCompatActivity {
         }
     }
 
-
+    private void crest()
+    {
+        Intent intent = new Intent(signIn.this, RegisterActivity.class);
+        startActivity(intent);
+    }
 
 
 }
