bf4884dd71cef552e89daaa54dd3541b52a87dcf
Aleksa <aleksr@sfu.ca>
2020-02-08 17:18:43 -0800

    added friendlist button fixed

--- app/src/main/java/com/example/systemhacks2020proj/FriendList.java
+++ app/src/main/java/com/example/systemhacks2020proj/FriendList.java
@@ -28,7 +28,7 @@ public class FriendList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_friend_list);
         myDialog = new Dialog(this);
 
         friendbutton = findViewById(R.id.addfriendbutton);
